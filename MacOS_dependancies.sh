300b9606ba0471fabc60be41ba6d1a0b94b4c94f
Ryan Ingalsbe <ryaningalsbe@Ryans-MBP.buffalo.rr.com>
2024-04-02 10:09:20 -0400

    DojoAssistant

--- MacOS_dependancies.sh
+++ MacOS_dependancies.sh
@@ -6,7 +6,11 @@ brew install portaudio
 pip install virtualenv
 python3 -m venv KenpoDojo
 mv main.py KenpoDojo/
+mv main2.py KenpoDojo/
 mv require.txt KenpoDojo/
 mv techniques.txt KenpoDojo/
+mv intro.mp3 KenpoDojo/
+mv outro.mp3 KenpoDojo/
+mv README.md KenpoDojo/
 cd KenpoDojo/
 pip install -r require.txt
